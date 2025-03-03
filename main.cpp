9ed9ea160c3715170fb5c31222bf8e5c72770933
Tạ Quang Khôi <khoi0941@gmail.com>
2025-03-03 21:37:46 +0700

    Done play ground for ball

--- main.cpp
+++ main.cpp
@@ -1,16 +1,62 @@
 #include <iostream>
 #include <raylib.h>
 
-int main()
-{
+int main() {
+    int ballX = 400;
+    int ballY = 400;
+    int speed = 3;
+    constexpr Color green = {20, 160, 133, 255};
+
     InitWindow(800, 800, "Keios Development");
 
-    // wait for awhile
-    while (!WindowShouldClose())
-    {
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) {
+        // 1. Event Handling
+        if (IsKeyDown(KEY_RIGHT)) {
+            ballX += speed;
+        }
+        if (IsKeyDown(KEY_LEFT)) {
+            ballX -= speed;
+        }
+        if (IsKeyDown(KEY_UP)) {
+            ballY -= speed;
+        }
+        if (IsKeyDown(KEY_DOWN)) {
+            ballY += speed;
+        }
+
+        // Edit Speed
+        if (IsKeyDown(KEY_A)) {
+            speed--;
+        }
+        if (IsKeyDown(KEY_S)) {
+            speed++;
+        }
+
+        // if ball at left edge of screen, move it to the right edge
+        if (ballX < 0) {
+            ballX = 800;
+        }
+        if (ballX > 800) {
+            ballX = 0;
+        }
+        if (ballY < 0) {
+            ballY = 800;
+        }
+        if (ballY > 800) {
+            ballY = 0;
+        }
+
+
+
+        // // 2. Updating Position
+
+
+        // 3. Drawing
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("Do Nguyen", 10, 10, 20, DARKGRAY);
+        ClearBackground(green);
+        DrawCircle(ballX, ballY, 20, WHITE);
         EndDrawing();
     }
 
