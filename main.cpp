43ca5174528c455b2d909da655f7067a95cf5a6b
Tạ Quang Khôi <khoi0941@gmail.com>
2025-03-03 20:42:45 +0700

    Show text in screen

--- main.cpp
+++ main.cpp
@@ -1,7 +1,19 @@
 #include <iostream>
+#include <raylib.h>
 
 int main()
 {
-    std::cout << "Hello, World!" << std::endl;
+    InitWindow(800, 800, "Keios Development");
+
+    // wait for awhile
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawText("Do Nguyen", 10, 10, 20, DARKGRAY);
+        EndDrawing();
+    }
+
+    CloseWindow();
     return 0;
 }
